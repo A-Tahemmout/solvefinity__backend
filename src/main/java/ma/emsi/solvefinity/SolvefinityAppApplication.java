8ef779798040dd25bca7907b420dbfb1a62b0da2
Tahemmout Aymane <Tahemmout.Aymane@outlook.com>
2023-01-12 11:56:29 +0100

    Init Backend

---
+++ src/main/java/ma/emsi/solvefinity/SolvefinityAppApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.solvefinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvefinityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolvefinityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/emsi/solvefinity/SolvefinityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.solvefinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvefinityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
